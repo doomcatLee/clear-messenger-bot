602d2475a981cb491cd79871d71c9b840b82a7a6
Glitch (clear-messenger-bot) <none>
2018-01-07 23:31:12 +0000

    🐆🍦 Updated with Glitch

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  app = express().use(bodyParser.json()), // creates express http server
+  request = require('request');
+
+// .env variables
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -21,11 +25,20 @@ app.post('/webhook', (req, res) => {
 		// Iterates over each entry - there may be multiple if batched
 		body.entry.forEach(function(entry) {
 
-			// Gets the message. entry.messaging is an array, but
-			// will only ever contain one message, so we get index 0
+      // This is the message object
 			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
+      let sender_psid = webhook_event.sender.id;		
+      
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
 		});
+    
+    
 
 		// Returns a '200 OK' response to all requests
 		res.status(200).send('EVENT_RECEIVED');
@@ -41,7 +54,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
 	// Your verify token. Should be a random string.
-	let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+	let VERIFY_TOKEN = "4880064052";
 
 	// Parse the query params
 	let mode = req.query['hub.mode'];
@@ -64,3 +77,53 @@ app.get('/webhook', (req, res) => {
 		}
 	}
 });
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
